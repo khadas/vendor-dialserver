2fa0838a2eb02994dba61ff740cff71aade1dad3
feng.wang1 <feng.wang1@amlogic.com>
2022-03-03 17:28:02 +0800

    yocto: specific request parameters Test Fail [1/2]
    PD#SWPL-73678
    
    Problem:
    specific request parameters Test Fail
    
    Solution:
    modify the youtube launch mode in launcher
    
    Verify:
    s4-ap222
    
    Signed-off-by: feng.wang1 <feng.wang1@amlogic.com>
    Change-Id: Ief48d32cd0cc4a71ff4e8ad84d7875aac9f30906

--- thunder_api.cpp
+++ thunder_api.cpp
@@ -50,7 +50,8 @@ int activateApp(const char *callsign, const char *url) {
     // Config the app to be activated
     if (!strcmp(curAppForDial->handler, "YouTube")) {
       JsonObject launchtype = JsonObject("{\"launchtype\": \"launch=dial\"}");
-      if (!strcmp(curAppForDial->name, "YouTubeTV") || !strcmp(curAppForDial->name, "YouTubeKids")) {
+      if ((!strcmp(curAppForDial->name, "YouTubeTV") || !strcmp(curAppForDial->name, "YouTubeKids"))
+          && (strstr(url,"loader=yts")!= nullptr)) {
         launchtype.Set("url", url);
       }
       ret = g_wpe_contoller->Set<JsonObject>(1000, std::string("configitem@") + callsign, launchtype);
